583ef23bb8455585805f10ef97979360ccf6a00b
Armin Aganbegovic <armin.ag@outlook.com>
2017-10-06 11:31:19 +0100

    Display Mock Tenants

---
+++ asp.net-core-multitenant/Controllers/TenantController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using asp.net_core_multitenant.Models;
+
+namespace asp.net_core_multitenant.Controllers
+{
+    public class TenantController : Controller
+    {
+        public IActionResult Index()
+        {
+            var tenants = new List<Tenant>
+            {
+                new Tenant
+                {
+                     Name = "Audi",
+                     DomainName = "www.audi.com",
+                     Id = 1,
+                     Default = true
+                },
+                new Tenant
+                {
+                     Name = "BMW",
+                     DomainName = "www.bmw.com",
+                     Id = 1,
+                     Default = false
+                },
+                new Tenant
+                {
+                     Name = "Porsche",
+                     DomainName = "www.porsche.com",
+                     Id = 1,
+                     Default = false
+                }
+            };
+            return View(tenants);
+        }
+    }
+}
---
+++ asp.net-core-multitenant/Models/Tenant.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace asp.net_core_multitenant.Models
+{
+    public class Tenant
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string DomainName { get; set; }
+        public bool Default { get; set; }
+    }
+}
