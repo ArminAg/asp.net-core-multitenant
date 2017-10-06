1dfc57be25472ebc8cf73a910b62800a59f1e42d
Armin Aganbegovic <armin.ag@outlook.com>
2017-10-06 11:42:01 +0100

    Add Session and Speaker Controllers and Models

---
+++ asp.net-core-multitenant/Models/Session.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace asp.net_core_multitenant.Models
+{
+    public class Session
+    {
+        public int Id { get; set; }
+        public int SessionId { get; set; }
+        public string Title { get; set; }
+        public string DescriptionShort { get; set; }
+        public string Description { get; set; }
+        public int PictureId { get; set; }
+
+        public Tenant Tenant { get; set; }
+        public List<Speaker> Speakers { get; set; }
+    }
+}
---
+++ asp.net-core-multitenant/Models/Speaker.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace asp.net_core_multitenant.Models
+{
+    public class Speaker
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string ImageUrl { get; set; }
+        public string WebSite { get; set; }
+        public string Bio { get; set; }
+        public bool AllowHtml { get; set; }
+        public int PictureId { get; set; }
+
+        public List<Session> Sessions { get; set; }
+    }
+}
